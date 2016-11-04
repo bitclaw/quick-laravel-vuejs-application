583731878ea48c0d4a6d3b32a34ad5e94e797ec0
Daniel C <bitclaw@gmail.com>
2016-11-04 14:22:02 -0500

    12-refactor-project-to-use-vue-1.0.28: Modify user table

--- app/User.php
+++ app/User.php
@@ -2,20 +2,43 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * App\User
+ *
+ * @mixin \Eloquent
+ * @property integer $id
+ * @property string $name
+ * @property string $email
+ * @property string $password
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property integer $role_level
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User wherePassword($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereRoleLevel($value)
+ * @property string $username
+ * @method static \Illuminate\Database\Query\Builder|\App\User whereUsername($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\User admin()
+ * @method static \Illuminate\Database\Query\Builder|\App\User notAdmin()
+ * @property-read \App\Role $role
+ */
 class User extends Authenticatable
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'username'
     ];
 
     /**
@@ -26,4 +49,22 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //RELATIONS
+    public function role(){
+        return $this->belongsTo(Role::class, 'role_level', 'level');
+    }
+
+    // Related attributes
+    public function isAdmin(){
+        return $this->role_level == 9;
+    }
+
+    //SCOPES
+    public function scopeAdmin(){
+        return $this->where('role_level', '=', 9);
+    }
+    public function scopeNotAdmin(){
+        return $this->where('role_level', '!=', 9);
+    }
 }
