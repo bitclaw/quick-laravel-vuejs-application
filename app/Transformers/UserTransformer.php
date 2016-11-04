d7d0f8cad38ccbe1a9063595ba79f5755d828919
Daniel C <bitclaw@gmail.com>
2016-11-04 14:25:01 -0500

    12-refactor-project-to-use-vue-1.0.28: Add transformers for user and role data

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Transformers;
+
+use App\User;
+use League\Fractal;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources possible to include
+     *
+     * @var  array
+     */
+    protected $availableIncludes = ['role'];
+
+    /**
+     * List of resources to automatically include
+     *
+     * @var  array
+     */
+    protected $defaultIncludes = [];
+
+    /**
+     * Transform object into a generic array
+     *
+     * @var  object
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return [
+            'name'     => $user->name,
+            'username' => $user->username,
+            'avatar'   => $user->avatar,
+            'email'    => $user->email
+        ];
+    }
+
+    public function includeRole(User $user)
+    {
+        return $this->item($user->role, new RoleTransformer);
+    }
+
+}
