c56c763e730cc45de98465c2a1da2b0b473e7200
Daniel C <bitclaw@gmail.com>
2016-11-04 14:20:42 -0500

    12-refactor-project-to-use-vue-1.0.28: Add Role eloquent model

---
+++ app/Role.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Role
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $level
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Role whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Role whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Role whereLevel($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Role whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class Role extends Model
+{
+    protected $fillable = ['name', 'level'];
+}
