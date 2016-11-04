d2c40b2fdd2564972a7ccf7218b1661fea373a38
Daniel C <bitclaw@gmail.com>
2016-11-04 14:25:21 -0500

    12-refactor-project-to-use-vue-1.0.28: Modify blade views

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,25 @@
-DASHBOARD TEST
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Admin Dashboard</title>
+    <link rel="shortcut icon" href="{{ config('website.icon') }}">
+
+</head>
+<body class="">
+
+<script>
+    @if(Auth::user())
+    const User = {!! Fractal::includes(['role'])->item(Auth::user(), new \App\Transformers\UserTransformer)->getJson() !!};
+    User.isAdmin = {{ Auth::user()->isAdmin() }};
+            @else
+                const User = {isAdmin: false}
+    @endif
+</script>
+
+</body>
+</html>
+
